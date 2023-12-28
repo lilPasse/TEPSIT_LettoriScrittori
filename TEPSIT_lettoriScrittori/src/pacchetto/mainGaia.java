36492ca8436d9987216f12b4f6dea95209911d88
gaiap <gaiap@192.168.1.86>
2023-12-28 16:08:48 +0100

    forth commit - Gaia

--- TEPSIT_lettoriScrittori/src/pacchetto/mainGaia.java
+++ TEPSIT_lettoriScrittori/src/pacchetto/mainGaia.java
@@ -2,6 +2,7 @@ package pacchetto;
 
 public class mainGaia {
 	public static void main(String[] args) {
-		System.out.print("Hello world");
+		System.out.print("Hello Matteo");
+		System.out.print("Hello Gaia");
 	}
 }
