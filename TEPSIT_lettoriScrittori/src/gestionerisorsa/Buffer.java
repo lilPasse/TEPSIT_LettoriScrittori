ab7cce44074b448b745884f6e7b41565f222166c
gaiap <gaiap@PCdiGaia.homenet.telecomitalia.it>
2024-01-03 17:46:56 +0100

    definizione approssimata di buffer e semaforo

---
+++ TEPSIT_lettoriScrittori/src/gestionerisorsa/Buffer.java
@@ -0,0 +1,17 @@
+package gestionerisorsa;
+
+public class Buffer {
+	
+	private String msg;
+	
+	public Buffer() {}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+}
---
+++ TEPSIT_lettoriScrittori/src/gestionerisorsa/Semaforo.java
@@ -0,0 +1,28 @@
+package gestionerisorsa;
+
+public class Semaforo {
+	
+	private int molteplicita;
+
+	public Semaforo() {}
+	public Semaforo(int n) {
+		this.molteplicita= n ;
+	}
+	
+	public synchronized void acquire() {
+	     while (molteplicita != 0) {		
+	        try {
+	            wait();
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt();
+	        }
+	     }
+	     molteplicita --;
+	 }
+
+	 public synchronized void release() {	 
+		molteplicita ++;;
+	    notify();
+	 }
+
+}
